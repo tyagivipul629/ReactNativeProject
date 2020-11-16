6314c3e73663807d4287b9200eadb3d32cbde1f2
tyagivipul629 <vipultyagi629@gmail.com>
2020-11-16 13:17:27 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,41 @@
+
+
+import React from 'react';
+import {
+  View,
+  Text,
+  TextInput
+} from 'react-native';
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      inptext:''
+    };
+  }
+  handleText=(text)=>{
+    this.setState({inptext:text});
+  }
+  render(){
+    return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+      }}>
+        <TextInput underlineColorAndroid = "transparent"
+               placeholder = "Email"
+               placeholderTextColor = "#9a73ef"
+               autoCapitalize = "none"
+               backgroundColor="yellow"
+               onChangeText={this.handleText} />
+      <Text>{this.state.inptext}</Text>
+      <Text>Helloj</Text>
+    </View>
+  );
+}
+};
+
+export default App;
